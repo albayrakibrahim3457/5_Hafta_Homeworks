c9d1931db02c67cafd1ef2a116004879ba2e9894
albayrakibrahim3457 <77547591+albayrakibrahim3457@users.noreply.github.com>
2023-03-20 16:28:06 +0300

    interface2

---
+++ Interfaces2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Update();
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+
+
+        }
+        interface IPersonManager
+        {
+            void Add();
+            void Update();
+        }
+        class CustomerManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Müşteri eklendi");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Müşteri güncellendi");
+            }
+        }
+        class EmployeeManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Personel eklendi");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Personel güncellendi");
+            }
+        }
+        class InternManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Stajyer eklendi");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Stajyer güncellendi");
+            }
+        }
+
+        class ProjectManager
+        {
+            public void Add(IPersonManager personManager)
+            {
+                personManager.Add();
+            }
+        }
+    }
+}
